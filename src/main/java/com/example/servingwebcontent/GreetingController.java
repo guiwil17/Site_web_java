aa298c6c3862676589d9f25a2309e5a7aa51c84a
Guillaume SOL <guillaume.sol@etudiant.univ-reims.fr>
2021-06-03 18:48:23 +0200

    Initial commit

---
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -0,0 +1,57 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@SessionAttributes("valueSession2")
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model, HttpSession session, @CookieValue(value = "foo", defaultValue = "hello") String fooCookie, HttpServletResponse response) {
+        model.addAttribute("name", name);
+        model.addAttribute("foo", fooCookie);
+        session.setAttribute("valueSession", 43);
+        response.addCookie(new Cookie("foo", "yaaaaaaaaaaaaaah"));
+        return "greeting";
+    }
+
+    @GetMapping("/testParam/{id}")
+    public String testParam(@PathVariable int id, Model model) {
+        model.addAttribute("id", id);
+        return "param";
+    }
+    private int value=42;
+    @ModelAttribute("value")
+    public int getValue()
+    {
+        return value;
+    }
+
+    private int valueSession2=42;
+    @ModelAttribute("valueSession2")
+    public int getValueSession2()
+    {
+        this.valueSession2 = 53;
+        return valueSession2;
+    }
+
+    @RequestMapping("/endsession")
+    public String endSession(SessionStatus status){
+        status.setComplete();
+        return "redirect:/greeting";
+    }
+
+    @ModelAttribute("someList")
+    public List<String> getSomeList(){
+        return Arrays.asList("Element 1", "Element 2", "Element 3");
+    }
+}
---
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -0,0 +1,15 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
